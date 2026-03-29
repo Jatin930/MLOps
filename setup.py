fab90c1975f70553c05caf2a7d0379a0d3e20a42
Jatin Talwar <jtalwar@nyc.yearup.org>
2026-03-29 17:56:51 -0400

    test

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="us_visa",
     version="0.1.0",
+    author="Jatin Talwar",
+    author_email="jt4u30@gmail.com",
     packages=find_packages(),
     install_requires=[],
 )
